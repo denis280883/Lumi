ec34b69f9a989ad64f61ab15322fe6efef424fae
denis280883 <denis_ge@yahoo.fr>
2015-08-29 20:27:52 +0200

    layout bootstrap

--- core/Controller.php
+++ core/Controller.php
@@ -11,7 +11,10 @@ class Controller{
 	public function render($view){
 		extract($this->vars);
 		$view = ROOT.DS.'view'.DS.$this->request->controller.DS.$view.'.php';
+		ob_start();
 		require($view);
+		$content_for_layout = ob_get_clean();
+		require ROOT.DS.'view'.DS.'layout'.DS.'default.php';
 	}
 
 	public function set($key,$value=null) {

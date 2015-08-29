7b23167005a2222647d70589f6cab3a7cc93ecaa
denis280883 <denis_ge@yahoo.fr>
2015-08-29 16:58:37 +0200

    Remove Bad PATH INFO

--- core/Request.php
+++ core/Request.php
@@ -4,7 +4,6 @@ class Request{
 	public $url;	// URL call by user
 
 	function __construct(){
-		//$this->url = isset($_SERVER'PATH_INFO'])?$_SERVER'PATH_INFO']:'/';//$_SERVER['PATH_INFO'];
 		$this->url = isset($_SERVER['PATH_INFO'])?$_SERVER['PATH_INFO']:'/';
  	}
 }

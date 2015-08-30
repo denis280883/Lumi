fb14e6e92b9647b1f074e0ab6c1b40c08f2a2947
denis280883 <denis_ge@yahoo.fr>
2015-08-31 01:28:59 +0200

    posts php
    INSERT INTO `lumitel`.`posts` (`id`, `name`, `content`, `created`,
    `online`, `type`, `slug`, `user_id`) VALUES (NULL, 'Ma première page',
    '<p>My Macbook was constantly nagging at me to ''service battery'', and
    even gave me a wizard to booking an appointment at an Apple Store to get
    it fixed. 2 hours later, I learned my computer was ''vintage'' (2009),
    so I bought the battery from iFixit.</p>', '2015-08-31 00:00:00', '1',
    'page', 'ma-premiere-page', '0');

--- controller/PagesController.php
+++ controller/PagesController.php
@@ -3,6 +3,11 @@ class PagesController extends Controller {
 
 	function view($id){
 		$this->loadModel('Post');
+		$posts = $this->Post->find(array(
+			'conditions' => 'id=1'
+
+		));
+		print_r($posts);
 	}
 
 

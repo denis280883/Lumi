2a14e88b3aa34c900883bc45efc8dd89b70cd280
denis280883 <denis_ge@yahoo.fr>
2015-08-31 02:16:50 +0200

    correct id condition
    INSERT INTO `lumitel`.`posts` (`id`, `name`, `content`, `created`,
    `online`, `type`, `slug`, `user_id`) VALUES (NULL, 'Ma seconde page',
    'Un second contenu bidon', '2015-08-31 00:00:00', '1', 'page',
    'ma-seconde-page', NULL);

--- controller/PagesController.php
+++ controller/PagesController.php
@@ -4,7 +4,7 @@ class PagesController extends Controller {
 	function view($id){
 		$this->loadModel('Post');
 		$post = $this->Post->findFirst(array(
-			'conditions' => 'id=1'
+			'conditions' => 'id='.$id
 		));
 		$this->set('post',$post);
 	}

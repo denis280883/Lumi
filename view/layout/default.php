41eedbb151c738205c9e4d0817bb610b5d94d708
denis280883 <denis_ge@yahoo.fr>
2015-08-31 22:57:33 +0200

    title site

--- view/layout/default.php
+++ view/layout/default.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr"> 
     <head> 
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/> 
-    <title></title>
+    <title><?php echo isset($title_for_layout)?$title_for_layout:'Mon site'; ?></title>
     <link rel="stylesheet" href="http://getbootstrap.com/2.3.2/assets/css/bootstrap.css">
      </head> 
     <body> 

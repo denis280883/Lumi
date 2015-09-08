a72b1f0d6f25b0cdf13be94193034c619a9d59b1
denis280883 <denis_ge@yahoo.fr>
2015-09-08 15:00:30 +0200

    Chrono to load page

--- webroot/index.php
+++ webroot/index.php
@@ -1,4 +1,5 @@
 <?php
+$begintime = microtime(true);
 define('WEBROOT',dirname(__FILE__));
 define('ROOT',dirname(WEBROOT));
 define('DS',DIRECTORY_SEPARATOR);
@@ -7,5 +8,10 @@ define('BASE_URL', dirname(dirname($_SERVER['SCRIPT_NAME'])));
 
 require CORE.DS.'includes.php';
 new Dispatcher();
-?>
 
+?>
+<div style="position:fixed;bottom:0; background:#900; color:#FFF; line-height:30px; height:30px; left:0; right:0; padding-left:10px; ">
+<?php
+echo 'Page générée en '. round(microtime(true)- $begintime, 5).' secondes';
+?>
+</div>

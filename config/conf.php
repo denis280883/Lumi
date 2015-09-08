ba00ac6572a1ea7884327d13262dddc6f0568b72
denis280883 <denis_ge@yahoo.fr>
2015-09-09 00:35:28 +0200

    Move router.php include

--- config/conf.php
+++ config/conf.php
@@ -18,4 +18,4 @@ class Conf{
 
 
 
-Router:connect('post/:slug-:id', 'posts/view/id:([0-9]+)/slug:([a-z0-9\-]+)')
+Router::connect('post/:slug-:id','posts/view/id:([0-9]+)/slug:([a-z0-9\-]+)');
--- core/includes.php
+++ core/includes.php
@@ -1,10 +1,11 @@
 <?php
 
 require 'functions.php';
+require 'Router.php';
 
 require ROOT.DS.'config'.DS.'conf.php';
+
 require 'Request.php';
-require 'Router.php';
 require 'Controller.php';
 require 'Model.php';
 require 'Dispatcher.php';

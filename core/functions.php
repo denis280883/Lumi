26f59b7b124f1d1151d5a29df97b86000713934c
denis280883 <denis_ge@yahoo.fr>
2015-09-08 14:48:03 +0200

    add if debug since file conf.php

--- core/functions.php
+++ core/functions.php
@@ -1,14 +1,15 @@
 <?php
 function debug($var){
-
-	$debug= debug_backtrace();
-	echo '<p>&nbsp;</p><p><a href="#"><strong>'.$debug[0]['file'].'</strong> 1.'.$debug[0]['line'].'</a></p>';
-	echo '<ol>';
-	foreach ($debug as $k=> $v) { if($k>0){
-		echo '<li><strong>'.$v['file'].' </strong> l.'.$v['line'].'</li>';
-	}}
-	echo '</ol>';
-	echo '<pre>';
-	print_r($var);
-	echo '</pre>';
+	if(Conf::$debug>0) {
+		$debug= debug_backtrace();
+		echo '<p>&nbsp;</p><p><a href="#" onclick="$(this).parent().next(\'ol\').slideToggle(); return false;"><strong>'.$debug[0]['file'].'</strong> 1.'.$debug[0]['line'].'</a></p>';
+		echo '<ol style="display:none;">';
+		foreach ($debug as $k=> $v) { if($k>0){
+			echo '<li><strong>'.$v['file'].' </strong> l.'.$v['line'].'</li>';
+		}}
+		echo '</ol>';
+		echo '<pre>';
+		print_r($var);
+		echo '</pre>';
+	}
 }

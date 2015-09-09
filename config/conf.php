ae9ebb2361f1e83e39c4a70d73593f7425e11aa1
denis280883 <denis_ge@yahoo.fr>
2015-09-09 23:45:52 +0200

    1h00 URL Global

--- config/conf.php
+++ config/conf.php
@@ -15,5 +15,5 @@ class Conf{
 
 }
 
-
-Router::connect('post/:slug-:id','posts/view/id:([0-9]+)/slug:([a-z0-9\-]+)');
+Router::connect('blog/:action','posts/:action');
+//Router::connect('post/:slug-:id','posts/view/id:([0-9]+)/slug:([a-z0-9\-]+)');

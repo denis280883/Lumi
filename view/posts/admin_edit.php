ad5486be56a22d65897c1e3a2f92b88da42d7543
denis280883 <denis_ge@yahoo.fr>
2015-09-11 00:31:46 +0200

    edit with Form

--- view/posts/admin_edit.php
+++ view/posts/admin_edit.php
@@ -3,10 +3,5 @@
 </div>
 
 <form action="<?php echo Router::url('admin/posts/edit'); ?>">
-	<div class="clearfix">
-		<label for="inputTitre">Titre</label>
-		<div class="input">
-			<input type="text" id="inputTitre" name="name" value="<?php echo $post->name; ?>">
-		</div>
-	</div>
+	<?php echo $this->Form->input('name', 'titre') ?>
 </form>

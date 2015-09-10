b13c25b83281eb3558ab294911437128442c449a
denis280883 <denis_ge@yahoo.fr>
2015-09-11 01:24:45 +0200

    attr

--- core/Form.php
+++ core/Form.php
@@ -11,10 +11,16 @@ class Form{
 		$html ='<div class="clearfix">
 					<label for="input'.$name.'">'.$label.'</label>
 					<div class="input">';
+		$attr = ' ';
+		foreach ($options as $k=>$v) {
+			if($k!='type'){
+				$attr .= "$k=\"$v\"";
+			}
+		}
 		if(!isset($options['type'])){
-			$html .= '<input type="text" id="input'.$name.'" name="'.$name.'" value="'.$this->controller->request->data->$name.'">';
+			$html .= '<input type="text" id="input'.$name.'" name="'.$name.'" value="'.$this->controller->request->data->$name.'" '.$attr.'>';
 		}elseif($options['type'] == 'textarea') {
-			$html .= '<textarea id="input'.$name.'" name="'.$name.'">'.$this->controller->request->data->$name.'</textarea>';
+			$html .= '<textarea id="input'.$name.'" name="'.$name.''.$attr.'>'.$this->controller->request->data->$name.'</textarea>';
 		}
 		$html .= '</div></div>';
 		return $html;

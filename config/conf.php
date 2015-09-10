0bf0731e8a70587e7d69ad268e52eef0766b8309
denis280883 <denis_ge@yahoo.fr>
2015-09-10 14:43:41 +0200

    Global URL

--- config/conf.php
+++ config/conf.php
@@ -18,4 +18,4 @@ class Conf{
 
 //Router::prefix('cockpit', 'admin');
 Router::connect('blog/:action','posts/:action');
-Router::connect('post/:slug-:id','posts/view/id:([0-9]+)/slug:([a-z0-9\-]+)');
+//Router::connect('post/:slug-:id','posts/view/id:([0-9]+)/slug:([a-z0-9\-]+)');

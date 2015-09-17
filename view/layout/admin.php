8574d634ca58b6f9b7097aa5179cca72918ed428
denis280883 <denis_ge@yahoo.fr>
2015-09-17 23:24:07 +0200

    Change Version tinymce 4 -> 3

--- view/layout/admin.php
+++ view/layout/admin.php
@@ -32,19 +32,49 @@
 
     </body> 
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
-    <script type="text/javascript" src="<?php echo Router::webroot('js/tinymce/tinymce.min.js'); ?>"></script>
-    <script type="text/javascript">
-    tinymce.init({
-        mode: "specific_textareas",
-        editor_selector: "wysiwyg",
-        plugins: [
-            "advlist autolink lists link image charmap print preview anchor",
-            "searchreplace visualblocks code fullscreen",
-            "insertdatetime media table contextmenu paste"
-        ],
-        toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
+    <script type="text/javascript" src="<?php echo Router::webroot('js/tinymce/tiny_mce.js'); ?>"></script>
+<script type="text/javascript">
+
+      tinyMCE.init({
+        // General options
+        mode : "specific_textareas",
+        editor_selector : "wysiwyg",
+        theme : "advanced",
+        relative_urls : false,
+        plugins : "autolink,lists,spellchecker,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
+     
+        // Theme options
+        theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,bullist,numlist,blockquote,|,justifyleft,justifycenter,justifyright,justifyfull,|,link,unlink,image,|,formatselect,fullscreen,code,ibrowser",
+
+        theme_advanced_buttons2 : "",
+        theme_advanced_buttons3 : "",
+        theme_advanced_buttons4 : "",
+        theme_advanced_toolbar_location : "top",
+        theme_advanced_toolbar_align : "left",
+        theme_advanced_statusbar_location : "bottom",
+        theme_advanced_resizing : true,
+
+        // Skin options
+        skin : "o2k7",
+        skin_variant : "silver",
+        file_browser_callback : 'fileBrowser'
     });
-    </script>
 
+    function fileBrowser(field_name, url, type, win){
 
+          tinyMCE.activeEditor.windowManager.open({
+            file : 'http://www.prodimex.ch/',
+            title : 'Gallerie',
+            width: 420,
+            height: 400,
+            resizable : 'yes',
+            inline : 'yes',
+            close_previous : 'no'
+          },{
+            window : win,
+            input : field_name
+          });
+          return false; 
+    }
+</script>
 </html>

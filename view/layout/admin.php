096c93ed00cf681358688d5e2fe9b737b896c6bb
denis280883 <denis_ge@yahoo.fr>
2015-09-17 18:46:24 +0200

    Add Script tinymce

--- view/layout/admin.php
+++ view/layout/admin.php
@@ -32,4 +32,5 @@
 
     </body> 
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+    <script type="text/javascript" src="<?php echo Router::webroot('js/tinymce/tiny_mce.js'); ?>"></script>
 </html>

baf65dcdaa7859545e17b5b95c78f9b3bda9f421
denis280883 <denis_ge@yahoo.fr>
2015-09-20 22:21:50 +0200

    Button Connect
    http://localhost/phpmyadmin
    
    Insert admin admin SHA1

--- view/users/login.php
+++ view/users/login.php
@@ -4,4 +4,7 @@
 	<form action= "<?php echo Router::url('users/login'); ?>" method="post">
 		<?php  echo $this->Form->input('login', 'Identifiant'); ?>
 		<?php  echo $this->Form->input('password', 'Mot de passe',array('type'=>'password')); ?>
+		<div class="action">
+			<input type="submit" class="btn primary" value="Se connecter">
+		</div>
 	</form>

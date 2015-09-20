8b01c638b15cb17f8e3c78d1931b87d21fb16488
denis280883 <denis_ge@yahoo.fr>
2015-09-20 22:16:00 +0200

    Correct div login.php

--- view/users/login.php
+++ view/users/login.php
@@ -1,7 +1,7 @@
 <div class="pages-header">
 	<h1>Zone réservé</h1>	
+</div>	
 	<form action= "<?php echo Router::url('users/login'); ?>" method="post">
 		<?php  echo $this->Form->input('login', 'Identifiant'); ?>
 		<?php  echo $this->Form->input('password', 'Mot de passe',array('type'=>'password')); ?>
 	</form>
-</div>

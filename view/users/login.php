12baeaf60579f35c96eda00fbf7d86c4593d54ac
denis280883 <denis_ge@yahoo.fr>
2015-09-20 22:04:52 +0200

    View Login

---
+++ view/users/login.php
@@ -0,0 +1,7 @@
+<div class="pages-header">
+	<h1>Zone réservé</h1>	
+	<form action= "<?php echo Router::url('users/login'); ?>" method="post">
+		<?php  echo $this->Form->input('login', 'Identifiant'); ?>
+		<?php  echo $this->Form->input('password', 'Mot de passe'); ?>
+	</form>
+</div>

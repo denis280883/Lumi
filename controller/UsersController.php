8e5498045147165f21dc7684f2ca6df019f89d72
denis280883 <denis_ge@yahoo.fr>
2015-09-20 23:19:12 +0200

    Compare user in database
    search in database content user and password to compare with Form
    
    admin
    admin
    
    admin
    123

--- controller/UsersController.php
+++ controller/UsersController.php
@@ -8,6 +8,12 @@ class UsersController extends Controller{
 		if($this->request->data){
 			$data=$this->request->data;
 			$data->password = sha1($data->password);
+			$this->loadModel('User');
+			$user = $this->User->findFirst(array('conditions' => array('login'=>$data->login, 'password' => $data->password
+				)));
+			if(!empty($user)){
+				debug($user);
+			}
 			debug($data);
 		}
 	}

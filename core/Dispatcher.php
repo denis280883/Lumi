beb44b4547b182ddceac033fd0c7df53bb0512a8
denis280883 <denis_ge@yahoo.fr>
2015-09-21 02:08:36 +0200

    Controller not existing
    http://localhost/Lumi/fsdafdf/

--- core/Dispatcher.php
+++ core/Dispatcher.php
@@ -40,6 +40,9 @@ class Dispatcher{
  	function loadController(){
  		$name = ucfirst($this->request->controller).'Controller';
  		$file = ROOT.DS.'controller'.DS.$name.'.php';
+ 		if(!file_exists($file)){
+ 			$this->error('Le controller '.$this->request->controller.' n\'existe pas');
+ 		}
  		require $file; 
  		$controller = new $name($this->request);
  		return $controller;

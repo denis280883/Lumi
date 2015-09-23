b56688f8b7d50c470912aa55bf8f8823145616b7
denis280883 <denis_ge@yahoo.fr>
2015-09-23 13:10:37 +0200

    Bootstrap 3

--- view/home/index.php
+++ view/home/index.php
@@ -1,6 +1,3 @@
-<!--start: Wrapper-->
-<div id="wrapper">
-
     <!--start: Container -->
     <div class="container">
 
@@ -11,44 +8,64 @@
                 <ul class="slides">
 
                     <li class="" style="width: 100%; float: left; margin-right: -100%; position: relative; display: none;">
-                        <img src="./webroot/imag/erp.png" alt="ERP">
+                        <img src="./webroot/img/services/01electricity.jpg" alt="ELECTRICITY">
 
                         <a href="http://www.1000ordi.ch">
                             <div class="slide-caption">
-                                <h2>ERP</h2>
+                                <h2>Electricité</h2>
                             </div>
                         </a>
                     </li>
 
                     <li style="width: 100%; float: left; margin-right: -100%; position: relative; display: none;" class="">
-                        <img src="./webroot/imag/business_infrastructure.png" alt="Business Infrastructure">
+                        <img src="./webroot/img/services/02diagnostic-repair.jpg" alt="diagnostic-repair">
 
                         <a href="http://www.prodimex.ch">
                             <div class="slide-caption">
-                                <h2>Business Infrastructure</h2>
+                                <h2>Service dépannage</h2>
                             </div>
                         </a>
                     </li>
 
                     <li style="width: 100%; float: left; margin-right: -100%; position: relative; display: none;" class="">
-                        <img src="./webroot/imag/vysual.png" alt="vysual">
+                        <img src="./webroot/img/services/03phone.png" alt="Telephony">
 
                         <a href="http://www.yahoo.fr">
                             <div class="slide-caption">
-                                <h2>vysual</h2>
+                                <h2>Téléphonie</h2>
                             </div>
                         </a>
                     </li>
 
                     <li style="width: 100%; float: left; margin-right: -100%; position: relative; display: none;" class="">
-                        <img src="./webroot/imag/individuals.png" alt="Particuliers">
+                        <img src="./webroot/img/services/04network.jpg" alt="Network">
 
                         <a href="http://www.lumitel.ch">
                             <div class="slide-caption">
-                                <h2>Particuliers</h2>
+                                <h2>Réseau informatique</h2>
                             </div>
                         </a>
                     </li>
+                    <li style="width: 100%; float: left; margin-right: -100%; position: relative; display: none;" class="">
+                        <img src="./webroot/img/services/05alarm.png" alt="security">
+
+                        <a href="http://www.lumitel.ch">
+                            <div class="slide-caption">
+                                <h2>Alarme et code d'accès</h2>
+                            </div>
+                        </a>
+                    </li>
+                    <li style="width: 100%; float: left; margin-right: -100%; position: relative; display: none;" class="">
+                        <img src="./webroot/img/services/06erp.png" alt="security">
+
+                        <a href="http://www.lumitel.ch">
+                            <div class="slide-caption">
+                                <h2>Alarme et code d'accès</h2>
+                            </div>
+                        </a>
+                    </li>
+
+
 
                 </ul>
             </div>
@@ -57,8 +74,66 @@
         <!-- end: Flexslider -->
 
 
-    </div>
-    <!--end: Container-->
+        <hr>
+
+        <div class="jumbotron">
+            <h3>
+                Smart is incredibly responsive, with a refreshingly clean responsive design and it has some awesome features, panorama sliders, 100+ icons, 50+ social icons, fancybox, isotope, google maps, facebook open graph and many, many more!
+            </h3>
+            <p><a class="btn btn-primary btn-lg">Learn more »</a></p>
+        </div><!--/.jumbotron-->
+
+
+        <!-- start: Row -->
+        <div class="row">
+
+            <!-- start: Icon Boxes -->
+            <div class="icons-box-vert-container">
+
+                <!-- start: Icon Box Start -->
+                <div class="offset1 span4">
+                    <div class="icons-box-vert">
+                        <i class="ico-ok ico-white circle-color-full"></i>
+
+                        <div class="icons-box-vert-info">
+                            <h3>Flexibilité</h3>
+
+                            <p>
+                                Nous répondons à toutes vos attentes, tout en vous conseillant, afin de trouver la
+                                meilleure solution
+                            </p>
+                        </div>
+                        <div class="clear"></div>
+                    </div>
+                </div>
+                <!-- end: Icon Box-->
+
+                <!-- start: Icon Box Start -->
+                <div class="offset2 span4">
+                    <div class="icons-box-vert">
+                        <i class="ico-ok ico-white circle-color-full"></i>
+
+                        <div class="icons-box-vert-info">
+                            <h3>Fiabilité</h3>
+
+                            <p>
+                                Nous travaillons avec une gamme de produit de haute qualité provenant de partenaires
+                                reconnus
+                            </p>
+                        </div>
+                        <div class="clear"></div>
+                    </div>
+                </div>
+                <!-- end: Icon Box -->
+
+            </div>
+            <!-- end: Icon Boxes -->
+            <div class="clear"></div>
+        </div>
+        <!-- end: Row -->
+
+
+
 
 </div>
 <!-- end: Wrapper  --><!--end: Content-->
